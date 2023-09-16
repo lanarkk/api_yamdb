13e2d0e346489cadc9d76467e2851e416ae9b171
Dmitrii Zhadaev <zhadaev1992@yandex.ru>
2023-09-16 23:52:31 +0300

    прикрутил пермишены

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
 
 from api.serializers import UserSerializer
+from api_yamdb.api.permissions import IsAdmin
 
 User = get_user_model()
 
