37a602acfa37d8ebc5c4f871977371d49b889795
liliia_kostyreva <kostyle@inbox.ru>
2023-09-18 01:45:05 +0300

    Добавлен импорт данных с помощью команды python manage.py load_csv_data

--- api_yamdb/reviews/management/commands/load_csv_data.py
+++ api_yamdb/reviews/management/commands/load_csv_data.py
@@ -2,11 +2,11 @@ import os
 from collections import OrderedDict
 from csv import DictReader
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 from django.shortcuts import get_object_or_404
 
 from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
-# from users.models import User
 
 ALREDY_LOADED_ERROR_MESSAGE = """
 Для загрузки данных из CSV-файла
@@ -14,11 +14,14 @@ ALREDY_LOADED_ERROR_MESSAGE = """
 После этого запустите команду `python manage.py migrate`
 для создания пустой БД.
 """
-IMOPRT_MESSAGE = """
+IMPORT_MESSAGE = """
 Импорт данных в таблицу {}
 --------------------------------------------------------------------------"""
 PATH_TO_FILES = settings.BASE_DIR / 'static/data/'
-FILES_VS_MODELS = OrderedDict(
+
+# Переменная, связывающая имена csv-файлов с таблицами.
+FILES_TO_MODELS = OrderedDict(
+    users=get_user_model(),
     category=Category,
     genre=Genre,
     titles=Title,
@@ -28,51 +31,54 @@ FILES_VS_MODELS = OrderedDict(
 )
 
 
-def get_csv_field_names(row_names, field_names):
-    csv_fields = {}
-    for field in field_names:
-        matches = list(filter(lambda s: field in s, row_names))
-        if len(matches) == 1:
-            csv_fields[field] = matches[0]
-        else:
-            raise KeyError(
-                f'В CSV-файле отсутствуе столбец для поля {field}.'
-            )
-    return csv_fields
-
-
 class Command(BaseCommand):
-    """Создает команду для командной строки для импорта данных из csv."""
+    """Создает команду терминала для импорта данных из csv.
+
+    Для запуска импорта:
+    python manage.py load_csv_data
+    """
 
     help = 'Загрузка данных из .csv файлов.'
 
     def handle(self, *args, **options):
-        for model in FILES_VS_MODELS.values():
+        """Переопределение метода класса, выполняющего основные действия."""
+
+        # Проверка того, не существуют ли уже заполненные таблицы
+        for model in FILES_TO_MODELS.values():
             if model.objects.exists():
                 return ALREDY_LOADED_ERROR_MESSAGE
 
-    for file in FILES_VS_MODELS:
-        filename = os.path.join(PATH_TO_FILES, file + '.csv')
-        model = FILES_VS_MODELS[file]
-        fk_fields = [
-            field for field in model._meta.fields if (
-                field.get_internal_type() == 'ForeignKey'
-            )
-        ]
+        # Цикл по csv-файлам, в порядке определеном упорядоченным словарем
+        # FILES_TO_MODELS
+        for file in FILES_TO_MODELS:
+            filename = os.path.join(PATH_TO_FILES, file + '.csv')
+            model = FILES_TO_MODELS[file]
 
-        with open(filename, encoding='utf-8') as csvfile:
-            print(IMOPRT_MESSAGE.format(model.__name__))
-            reader = DictReader(csvfile)
-            fields_are_consistent = set(reader.fieldnames).issubset(
-                set([field.name for field in model._meta.fields]))
+            # Определяем, есть ли в модели связанные поля.
+            fk_fields = [
+                field for field in model._meta.fields if (
+                    field.get_internal_type() == 'ForeignKey'
+                )
+            ]
 
-            for row in reader:
-                if fk_fields and fields_are_consistent:
-                    for field in fk_fields:
-                        row[field.name] = get_object_or_404(
-                            field.related_model,
-                            pk=row[field.name]
-                        )
-                model.objects.create(**row)
+            # Начинаем чтение файла
+            with open(filename, encoding='utf-8') as csvfile:
+                print(IMPORT_MESSAGE.format(model.__name__))
+                reader = DictReader(csvfile)
 
-        print(f'Число импортированных записей: {model.objects.count()}')
+                for row in reader:
+                    if fk_fields:
+                        for field in fk_fields:
+                            # Если поле связанной модели есть среди столбцов
+                            # csv-файла, то извлекаем объект связанной модели
+                            # по id из файла
+                            if field.name in reader.fieldnames:
+                                row[field.name] = get_object_or_404(
+                                    field.related_model,
+                                    pk=row[field.name]
+                                )
+                    model.objects.create(**row)
+
+            print(f'Число импортированных записей: {model.objects.count()}')
+
+        print('Импорт данных завершен.')
